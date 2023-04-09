d3984d25e7e4825dc38eca547d6cf3d077f8cd36
Fredrik000 <52358347+Fredrik000@users.noreply.github.com>
2023-04-09 10:11:59 +0200

    calculated total time from multiple adresses

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,16 +22,22 @@ import {
 const center = { lat: 59.910789, lng: 10.74989 };
 
 export default function Home() {
-  const [distance, setDistance] = useState('');
-  const [duration, setDuration] = useState('');
+  const [distance, setDistance] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const [directionsResponse, setDirectionsResponse] = useState(null);
   const [travelMode, setTravelMode] = useState('TRANSIT');
+  const [map, setMap] = useState(null);
 
   const destiantionRef = useRef();
   const originRef1 = useRef();
   const originRef2 = useRef();
   const originRef3 = useRef();
 
-  async function calculateRoute() {
+  let originRefs = [originRef1, originRef2, originRef3];
+
+  function calculateRoute() {
+    let calculatedDistance = 0;
+    let calculartedDuration = 0;
     if (
       originRef1.current.value === '' ||
       destiantionRef.current.value === ''
@@ -39,26 +45,35 @@ export default function Home() {
       return;
     }
 
-    const directionsService = new google.maps.DirectionsService();
-    const results = await directionsService.route({
-      origin: originRef1.current.value,
-      destination: destiantionRef.current.value,
-      travelMode: travelMode,
-    });
+    originRefs.map(async (originRef) => {
+      const directionsService = new google.maps.DirectionsService();
+      const results = await directionsService.route({
+        origin: originRef.current.value,
+        destination: destiantionRef.current.value,
+        travelMode: travelMode,
+      });
 
-    setDirectionsResponse(results);
-    setDistance(results.routes[0].legs[0].distance.text);
-    setDuration(results.routes[0].legs[0].duration.text);
+      // setDirectionsResponse(results);
+      calculatedDistance += results.routes[0].legs[0].distance.value;
+      calculartedDuration += results.routes[0].legs[0].duration.value;
+      setDistance(parseInt(calculatedDistance / 1000));
+      setDuration(parseInt(calculartedDuration / 60));
+
+      console.log(
+        `Current duration: ${results.routes[0].legs[0].duration.value}`
+      );
+      console.log(`Total duration: ${calculartedDuration}`);
+    });
   }
 
   function clearRoute() {
     setDirectionsResponse(null);
     setDistance('');
     setDuration('');
-    originRef1.current.value = '';
-    originRef2.current.value = '';
-    originRef3.current.value = '';
-    destiantionRef.current.value = '';
+    originRef1.current.value = 'Ladderudjordet 42, 2070 Råholt';
+    originRef2.current.value = 'Kongeveien 26, 0712 Oslo';
+    originRef3.current.value = 'Trollfaret 11A, 2020 Skedsmokorset';
+    destiantionRef.current.value = 'Fred Olsens Gate, 0154 Oslo';
   }
 
   const { isLoaded } = useJsApiLoader({
@@ -66,9 +81,6 @@ export default function Home() {
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
     libraries: ['places'],
   });
-
-  const [map, setMap] = useState(null);
-  const [directionsResponse, setDirectionsResponse] = useState(null);
 
   return (
     <>
@@ -100,57 +112,72 @@ export default function Home() {
           </Box>
         )}
 
-        {/* <MapForm setDirectionsResponse={setDirectionsResponse} /> */}
-        <Box
-          p={4}
-          borderRadius='lg'
-          m={4}
-          bgColor='white'
-          shadow='base'
-          minW='container.md'
-          zIndex='1'
-        >
-          <HStack spacing={2} justifyContent='space-between'>
-            <Box flexGrow={1}>
-              <div>
-                <Input type='text' placeholder='Origin' ref={originRef1} />
-              </div>
-            </Box>
-            <Box flexGrow={1}>
-              <div>
-                <Input
-                  type='text'
-                  placeholder='Destination'
-                  ref={destiantionRef}
-                />
-              </div>
-            </Box>
+        {isLoaded && (
+          <Box
+            p={4}
+            borderRadius='lg'
+            m={4}
+            bgColor='white'
+            shadow='base'
+            minW='container.md'
+            zIndex='1'
+          >
+            <HStack spacing={2} justifyContent='space-between'>
+              <Box flexGrow={1}>
+                <Autocomplete>
+                  <Input type='text' placeholder='Origin' ref={originRef1} />
+                </Autocomplete>
+              </Box>
+              <Box flexGrow={1}>
+                <Autocomplete>
+                  <Input type='text' placeholder='Origin' ref={originRef2} />
+                </Autocomplete>
+              </Box>
+              <Box flexGrow={1}>
+                <Autocomplete>
+                  <Input type='text' placeholder='Origin' ref={originRef3} />
+                </Autocomplete>
+              </Box>
+              <Box flexGrow={1}>
+                <Autocomplete>
+                  <Input
+                    type='text'
+                    placeholder='Destination'
+                    ref={destiantionRef}
+                  />
+                </Autocomplete>
+              </Box>
 
-            <ButtonGroup>
-              <Button colorScheme='pink' type='submit' onClick={calculateRoute}>
-                Calculate Route
-              </Button>
+              <ButtonGroup>
+                <Button
+                  colorScheme='pink'
+                  type='submit'
+                  onClick={calculateRoute}
+                >
+                  Calculate Route
+                </Button>
+                <IconButton
+                  aria-label='center back'
+                  icon={<FaTimes />}
+                  onClick={clearRoute}
+                />
+              </ButtonGroup>
+            </HStack>
+            <HStack spacing={4} mt={4} justifyContent='space-between'>
+              <Text>Distance: {distance} km</Text>
+              <Text>Duration: {duration} minutes</Text>
               <IconButton
                 aria-label='center back'
-                icon={<FaTimes />}
-                onClick={clearRoute}
+                icon={<FaLocationArrow />}
+                isRound
+                onClick={() => {
+                  map.panTo(center);
+                  map.setZoom(15);
+                }}
               />
-            </ButtonGroup>
-          </HStack>
-          <HStack spacing={4} mt={4} justifyContent='space-between'>
-            <Text>Distance: {distance} </Text>
-            <Text>Duration: {duration} </Text>
-            <IconButton
-              aria-label='center back'
-              icon={<FaLocationArrow />}
-              isRound
-              onClick={() => {
-                map.panTo(center);
-                map.setZoom(15);
-              }}
-            />
-          </HStack>
-        </Box>
+            </HStack>
+          </Box>
+        )}
       </Flex>
     </>
   );
